0d5c7240cad2b8b9f782bc92da95244601dcaa33
Mohan Prasath S <s.mohanprasath@gmail.com>
2023-11-13 14:49:42 +0530

    Added changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import * as React from "react";
 // import { fstore } from "./configs/firebase-config";
-import { Routes, Route, Navigate } from "react-router-dom";
+import {
+  Routes, Route,
+  // Navigate
+} from "react-router-dom";
 import HomePage from "./pages/home.page";
 import PortfolioTab from "./pages/tabs/portfolio.tab";
 
 function App() {
   return (
     <Routes>
-      <Route path="" element={<Navigate to="/dashboard" />} />
-      <Route path="/dashboard" element={<HomePage />} />
+      {/* <Route path="" element={<Navigate to="/dashboard" />} /> */}
+      <Route path="/" element={<HomePage />} />
       <Route path="/portfolio" element={<PortfolioTab />} />
     </Routes>
   );
